59a3adfde07a068be4798fda04d01edd55d57687
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2025-10-16 14:44:47 -0500

    Add a player class

---
+++ src/player.cpp
@@ -0,0 +1,13 @@
+#include "player.hpp"
+
+int Player::row() {
+  return current_row;
+}
+
+int Player::column() {
+  return current_column;
+}
+
+void Player::move_right() {
+  current_column += 1;
+}
